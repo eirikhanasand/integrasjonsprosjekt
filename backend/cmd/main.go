9deb5a9bd4fca6d14349c78b7524164508651cce
sindre <59136976+sindrelol@users.noreply.github.com>
2024-09-06 12:36:42 +0200

    Added backend files

---
+++ backend/cmd/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"integrasjon/service"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+type Struktern struct {
+	Name         string
+	RestaurantId string `bson:"restaurant_id,omitempty"`
+	Cuisine      string `bson:"cuisine,omitempty"`
+	Borough      string `bson:"borough,omitempty"`
+}
+
+func main() {
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	//use ../.env because main.go inside /cmd
+	if err = godotenv.Load(filepath.Join(pwd, ".env")); err != nil {
+		println(".env dir : " + filepath.Join(pwd, ".env"))
+		log.Println("No .env file found")
+		return
+	}
+	setup := service.SetupMongoStore()
+
+	if setup == false {
+		log.Fatalf("Failed to setup mongodb store.")
+		return
+	}
+
+	r := gin.Default()
+	r.GET("/", root)
+	r.Run()
+}
+
+func root(ctx *gin.Context) {
+	fetch, _ := service.FetchTypeFromKeyValue[Struktern]("restaurant_id", "2", "gubb")
+
+	ctx.JSON(http.StatusOK, fetch.Name)
+}
---
+++ backend/service/mongostore.go
@@ -0,0 +1,83 @@
+package service
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"os"
+	"time"
+)
+
+var database mongo.Database
+var ctx context.Context
+
+func SetupMongoStore() bool {
+	uri := os.Getenv("URI")
+	databaseName := os.Getenv("MONGO")
+
+	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+
+	client, clientErr := mongo.NewClient(options.Client().ApplyURI(uri))
+
+	if clientErr != nil {
+		log.Fatalf("failed to client %v", clientErr)
+		return false
+	}
+
+	err := client.Connect(ctx)
+
+	if err != nil {
+		log.Fatalf("failed to connect, %v", err)
+		return false
+	}
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Could not connect to the MongoDB server: ", err)
+	}
+
+	println(client.NumberSessionsInProgress())
+	database = *client.Database(databaseName, &options.DatabaseOptions{})
+	return true
+}
+
+func ReplaceWithKeyValue(key string, value string, doc any, collection string) bool {
+	col := database.Collection(collection)
+
+	_, err := col.ReplaceOne(ctx, bson.D{{key, value}}, doc)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func InsertDocument(doc any, collection string) bool {
+	col := database.Collection(collection)
+
+	_, err := col.InsertOne(ctx, doc)
+
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func FetchTypeFromKeyValue[T any](key string, value string, collection string) (*T, error) {
+	col := database.Collection(collection)
+
+	find := col.FindOne(ctx, bson.D{{key, value}})
+
+	if find.Err() != nil {
+		return nil, nil
+	}
+	var fetch T
+
+	err := find.Decode(&fetch)
+	if err != nil {
+		log.Fatalf("Failed to decode mongo document")
+		return nil, nil
+	}
+
+	return &fetch, nil
+}
