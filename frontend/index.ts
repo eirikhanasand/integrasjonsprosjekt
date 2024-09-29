8c1679794a4efd40962a6bc510404648ce544cb4
eirikhanasand <eirik.hanasand@gmail.com>
2024-09-29 16:29:49 +0200

    Moved App to src folder

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,5 +1,5 @@
 import registerRootComponent from 'expo/build/launch/registerRootComponent'
-import App from './App'
+import App from './src/App'
 
 // registerRootComponent calls AppRegistry.registerComponent("main", () => App)
 // It also ensures that whether you load the app in Expo Go or in a native build,
