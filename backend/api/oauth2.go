358a8c7ff5dff0752d456b232f79fbe11c102c65
Sindre Hagen Strømdal <sindrhst@stud.ntnu.no>
2024-09-30 21:11:26 +0200

    Added oauth2 support and docker files.

---
+++ backend/api/oauth2.go
@@ -0,0 +1,97 @@
+package api
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"net/http"
+)
+
+type DiscordInfo struct {
+	Id       string `json:"id"`
+	Username string `json:"username"`
+}
+
+type DiscordRequest struct {
+	Id string `form:"id"`
+}
+
+var stateMap = make(map[string]string)
+var DiscordMap = make(map[string]DiscordInfo)
+
+func (server *Server) DiscordLogin(ctx *gin.Context) {
+	var req DiscordRequest
+
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, "Malformed req request")
+		return
+	}
+
+	var state, _ = uuid.NewRandom()
+	stateMap[state.String()] = req.Id
+
+	ctx.JSON(http.StatusOK, server.Oauth2Config.AuthCodeURL(state.String()))
+}
+
+func (server *Server) DiscordCallback(ctx *gin.Context) {
+	value := ctx.Query("state")
+
+	id, found := stateMap[value]
+
+	if value == "" || found == false {
+		ctx.String(http.StatusBadRequest, "State mismatch")
+		return
+	}
+	defer delete(stateMap, value)
+
+	token, err := server.Oauth2Config.Exchange(context.Background(), ctx.Query("code"))
+
+	if err != nil {
+		ctx.String(http.StatusBadRequest, fmt.Sprintf("token error: %s", err.Error()))
+		return
+	}
+
+	res, err := server.Oauth2Config.Client(context.Background(), token).Get("https://discord.com/api/users/@me")
+
+	if err != nil || res.StatusCode < 200 || res.StatusCode > 200 {
+		if err != nil {
+			ctx.String(http.StatusInternalServerError, fmt.Sprintf("client error: %s", err.Error()))
+		} else {
+			ctx.String(res.StatusCode, "Discord error")
+		}
+		return
+	}
+
+	defer res.Body.Close()
+
+	var discordInfo DiscordInfo
+
+	err = json.NewDecoder(res.Body).Decode(&discordInfo)
+
+	if err != nil {
+		ctx.String(http.StatusInternalServerError, fmt.Sprintf("body error: %s", err.Error()))
+		return
+	}
+	DiscordMap[id] = discordInfo
+
+	ctx.JSON(http.StatusOK, discordInfo)
+}
+
+func (server *Server) GetDiscordInfo(ctx *gin.Context) {
+	var req DiscordRequest
+
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, "Malformed query request")
+		return
+	}
+
+	info, found := DiscordMap[req.Id]
+
+	if found == false {
+		ctx.JSON(http.StatusBadRequest, "Faulty id.")
+		return
+	}
+	ctx.JSON(http.StatusOK, info)
+}
--- backend/api/server.go
+++ backend/api/server.go
@@ -3,39 +3,57 @@ package api
 import (
 	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/ravener/discord-oauth2"
+	"golang.org/x/oauth2"
 	"os"
 )
 
 type Server struct {
 	UserCollection string
 	// TODO add more fields.
-	GameMap map[string]Game
+	GameMap      map[string]Game
+	Oauth2Config oauth2.Config
 }
 
 func (server *Server) InitServer() error {
-	if _, found := os.LookupEnv("user-collection"); found != false {
-		server.UserCollection = os.Getenv("user-collection")
+	if _, found := os.LookupEnv("USER_COLLECTION"); found != false {
+		server.UserCollection = os.Getenv("USER_COLLECTION")
 	} else {
 		return errors.New("no user-collection in the .env file")
 	}
+
+	server.Oauth2Config = oauth2.Config{
+		RedirectURL:  os.Getenv("LOGIN_REDIRECT"),
+		ClientID:     os.Getenv("CLIENT_ID"),
+		ClientSecret: os.Getenv("CLIENT_SECRET"),
+		Scopes:       []string{discord.ScopeIdentify},
+		Endpoint:     discord.Endpoint,
+	}
+
 	return nil
 }
 
 func (server *Server) StartServer() {
 	r := gin.Default()
 
-	users := r.Group("users")
+	api := r.Group("/api")
 	{
-		users.GET("", server.GetUsers)
-		users.POST("", server.PostUser)
-	}
+		api.GET("/login", server.DiscordLogin)
+		api.GET("/callback", server.DiscordCallback)
+		api.GET("/user", server.GetDiscordInfo)
 
-	game := r.Group("game")
-	{
-		game.GET("deaths", server.GetGameDeaths)
-		game.GET("scores", server.GetGameScores)
-	}
+		users := api.Group("/users")
+		{
+			users.GET("/", server.GetUsers)
+			users.POST("/", server.PostUser)
+		}
 
+		game := api.Group("/game")
+		{
+			game.GET("/deaths", server.GetGameDeaths)
+			game.GET("/scores", server.GetGameScores)
+		}
+	}
 	err := r.Run()
 
 	if err != nil {
