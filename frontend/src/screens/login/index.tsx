80ab1486c5e59a49038332d24cc5aa83c341899b
eirikhanasand <eirik.hanasand@gmail.com>
2024-09-30 23:55:49 +0200

    Added missing import

--- frontend/src/screens/login/index.tsx
+++ frontend/src/screens/login/index.tsx
@@ -1,6 +1,6 @@
 import GS from "@styles/globalStyles"
 import Swipe from "@components/nav/swipe"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { Text, TouchableOpacity, View } from "react-native"
 import handleLogin from "@components/login/login"
 
