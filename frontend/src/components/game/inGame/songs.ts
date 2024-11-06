2e84722f427489f528717c652a9e9fc67d4a5a43
eirikhanasand <eirik.hanasand@gmail.com>
2024-11-07 00:09:44 +0100

    Added music

--- frontend/src/components/game/inGame/songs.ts
+++ frontend/src/components/game/inGame/songs.ts
@@ -1,16 +1,94 @@
 const songs: Song[] = [
     {
-        uri: require('./assets/music/song1.mp4'),
-        nextSongs: ['./assets/music/song2.mp4', './assets/music/song3.mp4']
+        uri: require('@assets/music/ambient.mp3'),
+        nextSongs: [
+            '@assets/music/box.mp3', 
+            '@assets/music/bubbles.mp3',
+            '@assets/music/happy.mp3',
+            '@assets/music/cottage.mp3',
+        ]
     },
     {
-        uri: require('./assets/song2.mp4'),
-        nextSongs: ['./assets/song1.mp4', './assets/music/song3.mp4']
+        uri: require('./assets/music/box.mp3'),
+        nextSongs: [
+            '@assets/music/bubbles.mp3', 
+            '@assets/music/ambient.mp3',
+            '@assets/music/nature.mp3'
+        ]
     },
     {
-        uri: require('./assets/music/song3.mp4'),
-        nextSongs: ['./assets/music/song1.mp4', './assets/music/song2.mp4']
-    }
+        uri: require('./assets/music/bubbles.mp3'),
+        nextSongs: [
+            '@assets/music/nature.mp3', 
+            '@assets/music/cottage.mp3'
+        ]
+    },
+    {
+        uri: require('./assets/music/cottage.mp3'),
+        nextSongs: [
+            '@assets/music/nature.mp3', 
+            '@assets/music/ambient.mp3',
+            '@assets/music/box.mp3'
+        ]
+    },
+    {
+        uri: require('./assets/music/drone.mp3'),
+        nextSongs: [
+            '@assets/music/ambient.mp3',
+            '@assets/music/box.mp3',
+            '@assets/music/shark.mp3'
+        ]
+    },
+    {
+        uri: require('./assets/music/happy.mp3'),
+        nextSongs: [
+            '@assets/music/nature.mp3',
+            '@assets/music/ambient.mp3',
+            '@assets/music/bubbles.mp3',
+            '@assets/music/cottage.mp3',
+            '@assets/music/mysterious.mp3',
+        ]
+    },
+    {
+        uri: require('./assets/music/mysterious.mp3'),
+        nextSongs: [
+            '@assets/music/ambient.mp3',
+            '@assets/music/bubbles.mp3',
+            '@assets/music/cottage.mp3',
+        ]
+    },
+    {
+        uri: require('./assets/music/nature.mp3'),
+        nextSongs: [
+            '@assets/music/ambient.mp3',
+            '@assets/music/box.mp3',
+            '@assets/music/bubbles.mp3',
+            '@assets/music/mysterious.mp3',
+        ]
+    },
+    {
+        uri: require('./assets/music/shark.mp3'),
+        nextSongs: [
+            '@assets/music/story.mp3', 
+            '@assets/music/drone.mp3'
+        ]
+    },
+    {
+        uri: require('./assets/music/story.mp3'),
+        nextSongs: [
+            '@assets/music/nature.mp3', 
+            '@assets/music/ambient.mp3',
+            '@assets/music/bubbles.mp3',
+        ]
+    },
+    {
+        uri: require('./assets/music/upbeat.mp3'),
+        nextSongs: [
+            '@assets/music/ambient.mp3',
+            '@assets/music/nature.mp3',
+            '@assets/music/mysterious.mp3',
+        ]
+    },
 ]
 
 export default songs
